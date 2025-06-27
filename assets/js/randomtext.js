977af8fb03558240168df7f20cc2035b43034982
siobahnv <siobahnv@gmail.com>
2025-06-26 20:41:46 -0700

    Got the first line working as typewriter animate style.

--- assets/js/randomtext.js
+++ assets/js/randomtext.js
@@ -1,18 +1,80 @@
 
-function replaceMyElem(paramId, paramRandArr) {
+function getSlice(text, endpoint) {
+    return text.slice(0, endpoint);
+}
+
+function pickRandomTextFromArr(paramRandArr) {
+    const randomIndex = Math.floor(Math.random() * paramRandArr.length);
+    const randomWord = paramRandArr[randomIndex];
+    return randomWord;
+}
+
+function replaceMyElem(paramId, text) {
     const myElem = document.getElementById(paramId);
-    const randomIndex = Math.floor(Math.random() * paramRandArr.length); 
+    myElem.textContent = text;
+}
+
+// Eventually remove once update and replace code
+function replaceMyElemFromArr(paramId, paramRandArr) {
+    const myElem = document.getElementById(paramId);
+    const randomIndex = Math.floor(Math.random() * paramRandArr.length);
     const randomWord = paramRandArr[randomIndex];
     myElem.textContent = randomWord;
 }
 
-window.addEventListener('load', function () {
-    // Your code to execute after the page fully loads
-    const helloWorldID = "randomHelloWorld";
-    const helloWorlds = ["Hello, World Tree", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "مرحبا بالعالم", "你好世界", "안녕하세요, 세상", "Salve, Mundus"];
-    replaceMyElem(helloWorldID, helloWorlds);
+//////
 
+// Might take out...
+function blinkingCursor() {
+    const cursor = document.getElementById('cursor');
+    let isCursorVisible = true;
+
+    setInterval(() => {
+        if (isCursorVisible) {
+            cursor.style.opacity = 0;
+        } else {
+            cursor.style.opacity = 1;
+        }
+        isCursorVisible = !isCursorVisible;
+    }, 500); // Blinking speed in milliseconds
+}
+
+//////
+
+// This is where the action is
+window.addEventListener('load', function () {
+    // Bloody scoping...
+    // Your code to execute after the page fully loads
+    blinkingCursor();
+
+    // Print first line, "Hello World"
+    const helloWorldID = "randomHelloWorld";
+    const helloWorlds = ["Hello, World", "Hello, World Tree", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "مرحبا بالعالم", "你好世界", "안녕하세요, 세상", "Salve, Mundus"];
+    const randomText = pickRandomTextFromArr(helloWorlds);
+    // replaceMyElem(helloWorldID, helloWorlds);
+
+    let delay = 0;
+
+    for (let i = 0; i <= randomText.length; i++) {
+        setTimeout(function () {
+            const mySlice = getSlice(randomText, i);
+            console.log(`Iteration ${i}`);
+            console.log(mySlice);
+            console.log(delay);
+            replaceMyElem(helloWorldID, mySlice);
+            delay = 500 * i;
+        }, 500 * i); // Increase delay for each iteration
+    }
+    console.log(randomText);
+    console.log(helloWorldID.textContent);
+    console.log(delay);
+
+    // After printing first line, "Hello World"
+    // Print middle line
+
+
+    // Print last line
     const personalTitleID = "randomTitle";
     const personalTitles = ["a programmer", "a coder", "a web developer", "a fullstack engineer", "a support professional", "a tech junkie"];
-    replaceMyElem(personalTitleID, personalTitles);
+    replaceMyElemFromArr(personalTitleID, personalTitles);
 });
